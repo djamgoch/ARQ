4a4b7f5702cacecf3757a0bb83521135192111dd
Edwin Ho <eaho555@gmail.com>
2018-01-27 15:01:38 -0800

    Added Time.deltaTime to satellite rotation

--- ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
+++ ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class SatellitePlayerController : MonoBehaviour {
 
+    public const float SATELLITE_ROTATION_SPEED = 15f;
     public bool isBeingControlled = false;
 	
 	void Update () {
 		if (isBeingControlled)
         {
-            this.transform.Rotate(Vector3.forward, -Input.GetAxis("Horizontal"));
+            this.transform.Rotate(Vector3.forward, -Input.GetAxis("Horizontal") * Time.deltaTime * SATELLITE_ROTATION_SPEED);
         }
 	}
 
