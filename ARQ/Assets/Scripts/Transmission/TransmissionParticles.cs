23d01538a9855edd6acfa8c5992799125e66d4db
Edwin Ho <eaho555@gmail.com>
2018-01-27 13:18:56 -0800

    Added planet blocking puzzle and transmission particles asset

---
+++ ARQ/Assets/Scripts/Transmission/TransmissionParticles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransmissionParticles : MonoBehaviour {
+
+    public GameObject[] objectsToIgnore;
+
+    public void Initialize(GameObject[] objectsToIgnore)
+    {
+        foreach (var obj in objectsToIgnore)
+        {
+            //Physics.IgnoreCollision(this.GetComponent<ParticleSystem>(), obj.GetComponent<Collider>());
+        }
+    }
+}
