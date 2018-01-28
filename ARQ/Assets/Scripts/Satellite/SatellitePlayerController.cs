6d021168e91f4c562a8b6a2830ed1b0d1afdf66f
David Jamgochian <jammer888@verizon.net>
2018-01-27 19:43:34 -0800

    Added a limit to a satellite's rotation.
    Added a limit to a satellite's rotation.

--- ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
+++ ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
@@ -6,13 +6,45 @@ public class SatellitePlayerController : MonoBehaviour {
 
     public const float SATELLITE_ROTATION_SPEED = 15f;
     public bool isBeingControlled = false;
-	
+	public Transform planet;
+	public float lowerBoundAngle = -73.474f;
+	public float upperBoundAngle = 83.826f;
+
+	private float relativeZAngle;
+	private float normZAngle;
+
+	private void Start()
+	{
+		//this.transform.GetComponentInParent<Transform>().
+		Vector3 normDir = (this.transform.position - planet.position).normalized;
+		//normZAngle = Quaternion.Euler(normDir).eulerAngles.z;
+		Vector3 currDir = this.transform.up.normalized;
+		float relativeZAngle = Quaternion.FromToRotation(normDir, currDir).eulerAngles.normalized.z;
+		normZAngle = 0.0f;
+		Debug.Log(relativeZAngle);
+		//relativeZAngle = this.rotation
+		//relativeZAngle = 0.0f;
+	}
+
 	void Update () {
 		if (isBeingControlled)
         {
-			//Quaternion.Euler this.transform.rotation.z
-            this.transform.Rotate(Vector3.forward, -Input.GetAxis("Horizontal") * Time.deltaTime * SATELLITE_ROTATION_SPEED);
-        }
+			float zInput = -Input.GetAxis("Horizontal") * Time.deltaTime * SATELLITE_ROTATION_SPEED;
+			float newZAngle = relativeZAngle + zInput;
+			if (newZAngle > -70.0f && newZAngle < 70.0f)
+			{
+				this.transform.Rotate(Vector3.forward, zInput);
+				relativeZAngle = newZAngle;
+				//relativeZAngle += zInput;
+			}
+			//Debug.Log(this.name + ": current angle: " + (this.transform.localEulerAngles.z % 180) + ", new angle: " + newZAngle);
+			/*if (newZAngle > -lowerBoundAngle && newZAngle < upperBoundAngle)
+			{
+				this.transform.Rotate(Vector3.forward, zInput);
+				relativeZAngle += zInput;
+			}*/
+			//this.transform.Rotate(Vector3.forward, -Input.GetAxis("Horizontal") * Time.deltaTime * SATELLITE_ROTATION_SPEED);
+		}
 	}
 
     public void SetAsCurrentSatellite()
