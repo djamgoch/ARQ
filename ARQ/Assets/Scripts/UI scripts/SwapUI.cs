ba7b1c88d92d19f7b4d16bcff9fa82284c9d34c4
KTZimmerle <kzimmerl@uci.edu>
2018-01-27 17:22:11 -0800

    UISwap Script
    For MainMenu Synopsis

---
+++ ARQ/Assets/Scripts/UI scripts/SwapUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapUI : MonoBehaviour {
+
+    float timeToSwap;
+
+	// Use this for initialization
+	void Start () {
+        timeToSwap = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeToSwap < 0.0f)
+        {
+            transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeSelf);
+            transform.GetChild(1).gameObject.SetActive(!transform.GetChild(1).gameObject.activeSelf);
+            transform.GetChild(4).gameObject.SetActive(!transform.GetChild(4).gameObject.activeSelf);
+            timeToSwap = 10.0f;
+        }
+        else
+        {
+            timeToSwap -= Time.deltaTime;
+        }
+	}
+}
