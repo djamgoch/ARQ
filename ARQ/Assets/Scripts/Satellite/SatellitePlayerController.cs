a5b4ed3c45089833cf848a3eb48a7ba0e4d09db2
David Jamgochian <jammer888@verizon.net>
2018-01-28 11:50:14 -0800

    Made purposeful errors with satellite rotation.
    Made purposeful errors with satellite rotation and level 3, for testing
    purposes.

--- ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
+++ ARQ/Assets/Scripts/Satellite/SatellitePlayerController.cs
@@ -13,9 +13,14 @@ public class SatellitePlayerController : MonoBehaviour {
 
 	private void Start()
 	{
-		Vector3 normDir = (this.transform.position - planet.position).normalized;
+		//Vector3 normDir = (this.transform.position - planet.position).normalized;
+		Vector3 normDir = (planet.position - this.transform.position).normalized;
 		Vector3 currDir = this.transform.up.normalized;
-		relativeZAngle = Quaternion.FromToRotation(normDir, currDir).eulerAngles.z;
+		Debug.Log(this.transform.parent.name + ": normal vector = " + normDir);
+		Debug.Log(this.transform.parent.name + ": current vector = " + currDir);
+		//relativeZAngle = Quaternion.FromToRotation(normDir, currDir).eulerAngles.z;
+		relativeZAngle = Vector3.Angle(normDir, currDir);
+		Debug.Log(name + ": relative Z angle = " + relativeZAngle);          // -190
 		normZAngle = 0.0f;
 	}
 
@@ -23,11 +28,14 @@ public class SatellitePlayerController : MonoBehaviour {
 		if (isBeingControlled)
         {
 			float zInput = -Input.GetAxis("Horizontal") * Time.deltaTime * SATELLITE_ROTATION_SPEED;
-			float newZAngle = relativeZAngle + zInput;
-			if (newZAngle > -70.0f && newZAngle < 70.0f)
+			Vector3 normDir = (planet.position - this.transform.position).normalized;
+			Vector3 currDir = this.transform.up.normalized;
+			relativeZAngle = Vector3.Angle(normDir, currDir);
+			float newZAngle = relativeZAngle;
+			if (newZAngle >= -70.0f && newZAngle <= 70.0f)   // lower: -70, upper: 70
 			{
-				this.transform.Rotate(Vector3.forward, zInput);
-				relativeZAngle = newZAngle;
+				this.transform.Rotate(Vector3.forward, zInput, Space.World);
+				//relativeZAngle = newZAngle;
 			}
 		}
 	}
