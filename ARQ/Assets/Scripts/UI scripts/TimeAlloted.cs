3c6831675f7101ba068a175c50aca9c76311c709
KTZimmerle <kzimmerl@uci.edu>
2018-01-27 20:19:44 -0800

    Added TimeAlloted Script
    Using it to track how long a player has played in a level.

---
+++ ARQ/Assets/Scripts/UI scripts/TimeAlloted.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeAlloted : MonoBehaviour {
+
+    float time;
+    public Text timeAlloted;
+    bool isCountDown;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
